bcb53c640cd642616a76e4a3847603fad45273b2
josel <jos23limbert@gmail.com>
2025-12-10 18:12:02 -0400

    correcion guard auth

--- src/app/features/administration/presentation/guards/is-authenticated-guard.ts
+++ src/app/features/administration/presentation/guards/is-authenticated-guard.ts
@@ -1,16 +1,26 @@
 import { inject } from '@angular/core';
 import { Router, type CanActivateFn } from '@angular/router';
-import { tap } from 'rxjs';
+import { lastValueFrom, tap } from 'rxjs';
 import { AuthData } from '../../datasources/auth-data';
 import { environment } from '../../../../../environments/environment';
 
-export const isAuthenticatedGuard: CanActivateFn = (route, state) => {
+export const isAuthenticatedGuard: CanActivateFn =  (route, state) => {
   const authData = inject(AuthData);
   return authData.checkAuthStatus().pipe(
     tap((isAuth) => {
       if (!isAuth) {
+        console.log(isAuth);
         window.location.href = `${environment.baseUrl}/auth/test`
       }
     })
   );
+
+  // try {
+  //   await lastValueFrom(authData.checkAuthStatus());
+  //   return true;
+  // } catch (e) {
+  //   // 🔥 Navegación nativa → NO activa Angular Router → NO intenta parsear nada
+  //   window.location.href = '/api/auth/login';
+  //   return false;
+  // }
 };
